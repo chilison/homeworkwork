a8cf13918ed3b19e314e06a5419f82153f47dd87
chilison <91323484+chilison@users.noreply.github.com>
2021-10-22 00:07:53 +0300

    practice-2

---
+++ practice-2.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  practice-2
+//
+//  Created by Соня Артеменко on 15.10.2021.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include<cmath>
+#include <ctime>
+
+using namespace std;
+
+
+
+
+int main(){
+    void p151021z1();
+    void p151021z2();
+    void p151021z3();
+    void p151021z4();
+    p151021z4();
+}
+
