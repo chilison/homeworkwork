bfc13b970a15cb2d2d8d182eb662d5cff5b77527
chilison <91323484+chilison@users.noreply.github.com>
2021-11-18 21:48:28 +0300

    practice-8

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  practice-5
+//
+//  Created by Соня Артеменко on 12.11.2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    void z1();
+    void z2();
+    void z3();
+    void z4();
+    //z1();
+    //z2();
+    //z3();
+    //z4();
+    return 0;
+}
